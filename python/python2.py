b80b2aea7adeb23fe6836fd612fcdcb03835ee49
kyayonka1 <joelkyayonka@gmail.com>
2024-02-14 14:02:53 +0300

    assignment 11

---
+++ python/python2.py
@@ -0,0 +1,8 @@
+class Human:
+    def_init_(self, name, age, gender):
+        self.age=age
+        self.name=name
+        self.gender=gender
+
+        def introduce(self):
+            print(f"Hello,my name is (self.name), I'm {self.name} years old and my gender is {self.gender} ")
